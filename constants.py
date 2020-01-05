f2235cbd794c9cd835cd6d0a2a403d722b1cec17
Phoenix Clark <35511982+aeroplaneb29@users.noreply.github.com>
2020-01-04 19:18:10 -0700

    feature shooter functonality
    added velocity pid for the shooter

--- constants.py
+++ constants.py
@@ -1,2 +1,9 @@
 class Constants:
-    pass
+    SHOOTER_TALON_MAX_SPEED = 1000
+    TIMEOUT_MS = 20
+    SHOOTER_VELOCITY_F = 0
+    SHOOTER_VELOCITY_P = 0
+    SHOOTER_VELOCITY_I = 0
+    SHOOTER_VELOCITY_D = 0
+
+
--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -1,6 +1,8 @@
 from wpilib.command.subsystem import Subsystem
 from robotmap import RobotMap
 from ctre.wpi_talonsrx import WPI_TalonSRX
+from constants import Constants
+from ctre._impl import FeedbackDevice, ControlMode
 
 
 class Shooter(Subsystem):
@@ -8,3 +10,19 @@ class Shooter(Subsystem):
         super().__init__()
 
         self.shooterTalon = WPI_TalonSRX(RobotMap.SHOOTER_TALON)
+
+        self.shooterTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,
+                                                       Constants.TIMEOUT_MS)
+        self.configure_pid()
+
+    def configure_pid(self):
+        self.shooterTalon.config_kF(0, Constants.SHOOTER_VELOCITY_F, Constants.TIMEOUT_MS)
+        self.shooterTalon.config_kP(0, Constants.SHOOTER_VELOCITY_P, Constants.TIMEOUT_MS)
+        self.shooterTalon.config_kI(0, Constants.SHOOTER_VELOCITY_I, Constants.TIMEOUT_MS)
+        self.shooterTalon.config_kD(0, Constants.SHOOTER_VELOCITY_D, Constants.TIMEOUT_MS)
+
+    def motor_on(self):
+        self.shooterTalon.set(WPI_TalonSRX.ControlMode.Velocity, Constants.SHOOTER_TALON_MAX_SPEED)
+
+    def motor_off(self):
+        self.shooterTalon.set(0)
