4ad22ed6e9a37a548bc095e4ed8cf7da8f923acd
Devin Vander Stelt <devin@vstelt.com>
2020-01-06 13:55:28 -0700

    Changed drivetrain to tank drive for more realistic physics

--- physics.py
+++ physics.py
@@ -1,40 +1,28 @@
-import pprint
-
-from pyfrc.physics import drivetrains
-from pyfrc.sim import get_user_renderer
+from pyfrc.physics import motor_cfgs, tankmodel
+from pyfrc.physics.units import units
 
 
 class PhysicsEngine:
 
     def __init__(self, physics_controller):
         self.physics_controller = physics_controller
-        self.drivetrain = drivetrains.TwoMotorDrivetrain()
-        self.initial = True
-        self.count = 0
+
+        bumper_width = 3.25 * units.inch
+
+        self.drivetrain = tankmodel.TankModel.theory(
+            motor_config=motor_cfgs.MOTOR_CFG_CIM,
+            robot_mass=110 * units.lb,
+            gearing=10.71,
+            nmotors=2,
+            x_wheelbase=22 * units.inch,
+            robot_width=23 * units.inch + bumper_width * 2,
+            robot_length=32 * units.inch + bumper_width * 2,
+            wheel_diameter=6 * units.inch
+        )
 
     def update_sim(self, hal_data, now, tm_diff):
-        # print(hal_data)
-        # user_renderer = get_user_renderer()
-        # user_renderer.clear()
-        #
-        # if self.count % 3 == 0:
-        #     user_renderer.draw_line([(0, 0), (5, 5)], color='#0000ff', robot_coordinates=True, arrow=False)
-        # elif self.count % 3 == 1:
-        #     user_renderer.draw_line([(0, 0), (5, 5)], color='#00ff00', robot_coordinates=True, arrow=False)
-        # elif self.count % 3 == 2:
-        #     user_renderer.draw_line([(0, 0), (5, 5)], color='#ff0000', robot_coordinates=True, arrow=False)
-        #     self.count = -1
-
-        self.count += 1
-
-        if self.initial:
-            self.initial = False
-            # pprint.pprint(hal_data["CAN"])
-            # for key in hal_data["sparkmax-5"].keys():
-            #     print(key)
-        # Simulate the drivetrain
         left_master_motor = -hal_data["CAN"][1]["value"]
         right_master_motor = hal_data["CAN"][3]["value"]
 
-        speed, rotation = self.drivetrain.get_vector(left_master_motor, right_master_motor)
-        self.physics_controller.drive(speed, rotation, tm_diff)
+        x, y, angle = self.drivetrain.get_distance(left_master_motor, right_master_motor, tm_diff)
+        self.physics_controller.distance_drive(x, y, angle)
