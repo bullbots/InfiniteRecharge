a203da4ec27d8c158ef6ea8b3cfdfc733432492c
Phoenix Clark <35511982+aeroplaneb29@users.noreply.github.com>
2020-01-07 18:03:13 -0700

    corrections requested fixed

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -9,20 +9,20 @@ class Shooter(Subsystem):
     def __init__(self):
         super().__init__()
 
-        self.shooterTalon = WPI_TalonSRX(RobotMap.SHOOTER_TALON)
+        self.shooter_talon = WPI_TalonSRX(RobotMap.SHOOTER_TALON)
 
-        self.shooterTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,
-                                                       Constants.TIMEOUT_MS)
+        self.shooter_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,
+                                                        Constants.TIMEOUT_MS)
         self.configure_pid()
 
     def configure_pid(self):
-        self.shooterTalon.config_kF(0, Constants.SHOOTER_VELOCITY_F, Constants.TIMEOUT_MS)
-        self.shooterTalon.config_kP(0, Constants.SHOOTER_VELOCITY_P, Constants.TIMEOUT_MS)
-        self.shooterTalon.config_kI(0, Constants.SHOOTER_VELOCITY_I, Constants.TIMEOUT_MS)
-        self.shooterTalon.config_kD(0, Constants.SHOOTER_VELOCITY_D, Constants.TIMEOUT_MS)
+        self.shooter_talon.config_kF(0, Constants.SHOOTER_VELOCITY_F, Constants.TIMEOUT_MS)
+        self.shooter_talon.config_kP(0, Constants.SHOOTER_VELOCITY_P, Constants.TIMEOUT_MS)
+        self.shooter_talon.config_kI(0, Constants.SHOOTER_VELOCITY_I, Constants.TIMEOUT_MS)
+        self.shooter_talon.config_kD(0, Constants.SHOOTER_VELOCITY_D, Constants.TIMEOUT_MS)
 
     def motor_on(self):
-        self.shooterTalon.set(WPI_TalonSRX.ControlMode.Velocity, Constants.SHOOTER_TALON_MAX_SPEED)
+        self.shooter_talon.set(ControlMode.Velocity, Constants.SHOOTER_TALON_MAX_SPEED)
 
     def motor_off(self):
-        self.shooterTalon.set(0)
+        self.shooter_talon.set(0)
